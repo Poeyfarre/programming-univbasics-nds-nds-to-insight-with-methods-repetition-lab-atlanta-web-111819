49ddcfb1ac2a5cad6b4f33e2d7c13d2679c07ee8
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-14 18:21:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,15 +25,12 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
+  
   # Write this implementation
 end
 
 def total_gross(source)
-  list_of_directors = [ ]
-  row_count = 0 
-  while row_count < source.length do
-    list_of_directors << source[row_count][:name]
-    row_count += 1 
+  
 end
 list_of_directors
   # Write this implementation
